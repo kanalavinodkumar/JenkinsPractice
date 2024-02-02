c8634f9fd83c65068809e4b2c30ca35ecfcdfc4a
kanalavinodkumar <kanalavinodkumar@gmail.com>
2024-02-02 16:17:20 +0530

    initial commit

---
+++ Workstation.tf
@@ -0,0 +1,10 @@
+module "this" {
+  #source = "git::https://github.com/kanalavinodkumar/Docker-infra.git"
+  source = "K:/practice/Docker-infra"
+  cidr_block = var.cidr_block
+  common_tags = var.vpc_tags
+  public_subnet_cidr = var.public_subnet_cidr
+  public_subnet_name = var.public_subnet_name
+  private_subnet_cidr = var.private_subnet_cidr
+  private_subnet_name = var.private_subnet_name
+}
